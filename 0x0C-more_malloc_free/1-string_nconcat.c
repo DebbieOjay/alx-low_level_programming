080f6f079b1086192df4fa48e1efef910289e289
okachibe deborah <okachibedebbie@gmail.com>
2022-10-06 11:00:30 -0700

    more malloc solutions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,13 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
+	for (i = 0; s1[i]; i++)
+		;
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	for (j = 0; s2[j]; j++)
+		;
 
 	if (n < j)
 		j = n;
